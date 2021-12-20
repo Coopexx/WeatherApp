e0755ab4e5fbad3fac0774da1f689dd08367d8e6
Hendrik Treuner <hendrik.treuner@googlemail.com>
2021-12-20 20:40:25 +0100

    Added Styling

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const url =
-    'https://api.openweathermap.org/data/2.5/weather?q=Berlin,ger,uk&APPID=085956f708858f21a3465350a845fa1e&units=metric'
+const link =
+    'https://api.openweathermap.org/data/2.5/weather?q=Berlin,ger,uk&APPID='
 
-//I stored the data in an obj, but now I have to handle asynch await
-//So I need an asynch function, that fetches the data.
-//I need another asynch function, that calles the fetching data function
-//and that has an await statement that calls the next function which handles
-//the obj data that was send.
+const key = config.SECRET_API_KEY
+
+const url = link + key
 
 async function fetchData() {
     const response = await fetch(url)
