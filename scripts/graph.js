d6671f527cde76cf177be72488d1f150cb88a400
Hendrik Treuner <hendrik.treuner@googlemail.com>
2021-12-30 12:14:58 +0100

    Feature: Final API integration
    Integration and rendering of API data of the OpenWeatherAPI.
    
    Remove markup for displaying the name
    
    Remove parameter list for precipitation and wind, because this data is not supported by the OpenWeatherAPI

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,42 +1,53 @@
-const ctx = document.querySelector('.statistics__graph').getContext('2d')
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: [
-            '14:00',
-            '16:00',
-            '18:00',
-            '20:00',
-            '22:00',
-            '00:00',
-            '02:00',
-            '04:00',
-        ],
-        datasets: [
-            {
-                label: '',
-                data: [2, 0, -2, 0, 4, 2, -2, -2],
-                backgroundColor: ['#fff'],
-                borderColor: ['#fff'],
-                borderWidth: 3,
-            },
-        ],
-    },
-    options: {
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                display: false,
-            },
+const renderGraph = (dataObj) => {
+    dataObj.hourly.length = 16;
+    console.log(dataObj);
+
+    const labels = [];
+    const data = [];
+
+    for (let i = 0; i < dataObj.hourly.length; i += 2) {
+        labels.push(dataObj.hourly[i].hour + ':00');
+        data.push(Math.round(dataObj.hourly[i].temp));
+    }
+
+    const tempMin = Math.min(...data) - 2;
+    const tempMax = Math.max(...data) + 2;
+
+    const ctx = document.querySelector('.statistics__graph').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [
+                {
+                    label: '',
+                    data: data,
+                    backgroundColor: ['#fff'],
+                    borderColor: ['#fff'],
+                    borderWidth: 3,
+                },
+            ],
         },
-        scales: {
-            y: {
-                min: -4 /*Always 2 more or less than min and max*/,
-                max: 6,
-                ticks: {
-                    stepSize: 2,
+        options: {
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+            scales: {
+                y: {
+                    min: tempMin,
+                    max: tempMax,
+                    ticks: {
+                        stepSize: 2,
+                        callback: function (val) {
+                            return val + ' °';
+                        },
+                    },
                 },
             },
         },
-    },
-})
+    });
+};
